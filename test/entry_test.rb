547a66561125b63959d5a29338151ebcf678f699
timproctor <timandcamiproctor@gmail.com>
2014-06-23 14:50:41 -0600

    Adds a Rake task to run all tests and debugs test casing mismatch

--- test/entry_test.rb
+++ test/entry_test.rb
@@ -6,9 +6,9 @@ require 'pry'
 class EntryTest<Minitest::Test
 
   def data
-      {first_Name: "Allison", last_Name: "Larson", zipcode: "89009", homephone: "(454) 567-7890"}
-      # {first_Name: "Tim", last_Name: "Proctor", zipcode: "67897", phonenumber: "(123) 456-8908"},
-      # {first_Name: "John", last_Name: "Smith", zipcode: "12345", phonenumber: "(345) 456-7890"}
+    {first_name: "Allison", last_name: "Larson", zipcode: "89009", homephone: "(454) 567-7890"}
+    # {first_Name: "Tim", last_Name: "Proctor", zipcode: "67897", phonenumber: "(123) 456-8908"},
+    # {first_Name: "John", last_Name: "Smith", zipcode: "12345", phonenumber: "(345) 456-7890"}
   end
 
   def test_clean_incoming_data
@@ -27,7 +27,7 @@ class EntryTest<Minitest::Test
   def test_can_retrieve_first_Name_from_attendees
     entry = Entry.new(data)
 
-    assert_equal "Allison", entry.first_Name
+    assert_equal "Allison", entry.first_name
   end
 
 
